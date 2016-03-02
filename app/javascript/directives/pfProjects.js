e835e33dfec5dd39579ab9b930047d9424076944
Kasper Bruus Frank <kasperbfrank@gmail.com>
2016-03-02 16:36:54 +0100

    project hover animations done.

--- app/javascript/directives/pfProjects.js
+++ app/javascript/directives/pfProjects.js
@@ -6,12 +6,20 @@
         .directive('pfProjects', pfProjects);
 
     /* @ngInject */
-    function pfProjects() {
+    function pfProjects($location) {
         var directive = {
             restrict: 'EA',
-            templateUrl: 'partials/portfolio/projects.html'
+            templateUrl: 'partials/portfolio/projects.html',
+            link: linkFunc
         };
 
         return directive;
+
+        function linkFunc(scope, el, attr) {
+            scope.goToProject = function(projectName) {
+                $location.path('/portfolio/' + projectName);
+                // console.log('click');
+            }
+        }
     }
 })();
--- app/javascript/directives/scroll.js
+++ app/javascript/directives/scroll.js
@@ -29,7 +29,7 @@
                             if ((i % 2) && angular.element($window).width() > 991) {
                                 offset = '0px';
                             }
-                            jQuery(this).css({ top: offset }).delay(i * 200).animate({
+                            jQuery(this).delay(i * 200).animate({
                                 opacity: 1
                             }, 400);
                         });
