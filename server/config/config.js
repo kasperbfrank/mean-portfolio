0cecbdc0303ab9959261297763a141bfd36a53ce
Kasper Bruus Frank <kasperbfrank@gmail.com>
2016-04-05 15:14:35 +0200

    mongodb config.

--- server/config/config.js
+++ server/config/config.js
@@ -4,12 +4,12 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost/test', // switch this out for db....
+        db: 'mongodb://localhost/portfoliodb', // switch this out for db....
         port: process.env.PORT || 3030
     },
     production: {
         rootPath: rootPath,
-        db: 'mongodb://username:password@ds013619.mlab.com:13619/test', // switch this out for remote db....
+        db: 'mongodb://<username>:<password>@ds013619.mlab.com:13619/<dbname>', // switch this out for remote db....
         port: process.env.PORT || 80
     }
 }
